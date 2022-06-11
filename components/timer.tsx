a09262d9d719d4735b2a1f87d0eba1224d8bfae2
iskan.mustamir <iskan.mustamir@mail.ugm.ac.id>
2022-06-11 21:05:17 +0700

    feat: Add form

---
+++ components/timer.tsx
@@ -0,0 +1,56 @@
+import type { NextComponentType } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+
+const Timer: NextComponentType = () => {
+  const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("5/27/2022 21:44:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      {partyTime ? (
+        <div>Selesai 🎉</div>
+      ) : (
+        <div>
+          {days} : {hours} : {minutes} : {seconds}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Timer;
